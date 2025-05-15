d375684778e24a6c31f9ea75fc30aa329a091083
i-i-ijul <haqqikamvret@gmail.com>
2025-05-15 09:43:22 +0700

    first commit

---
+++ DoublyLinkedList.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node
+{
+public:
+    int noMhs;
+
+    Node *next;
+    Node *prev;
+};
+
+class doublelinkedlist{
+private:
+    Node *START;
+    
+};
+int main(){
+
+}
