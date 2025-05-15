e94cd1730200597dcc3734416fd45c16c22fd271
i-i-ijul <haqqikamvret@gmail.com>
2025-05-15 10:15:38 +0700

    memasukkan interger kedalam node

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -48,7 +48,29 @@ public:
             //step 5: START.prev = newNode (if START exists)
             if (START != NULL)
                 START->prev = newNode;
+            
+            //step 6: newNode.prev = NULL
+            newNode->prev = NULL;
+
+            //step 7: START = newNode
+            START = newNode;
+            return;
         }
+
+        //insert int between node
+        // step 8: locate position for insertion
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll numbers not allowed" << endl;
+            return;
+        }
+        
     }
 };
 int main(){
